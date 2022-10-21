14ebd3ea3870824da3334ada0f778be34086c9ad
Kaue77k <112183886+Kaue77k@users.noreply.github.com>
2022-10-21 12:05:17 -0300

    Add files via upload

---
+++ LaçoWile.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LaçoWile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner ler = new Scanner (System.in);
+		
+		double i=0;
+		
+		do {
+			i= i+2;
+			
+		}while (i<500);
+		
+	
+		
+		
+	}
+
+}

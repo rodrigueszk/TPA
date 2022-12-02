15a08fea4a1fced17021500edb904356efc53dfc
Kauê Rodrigues <112183886+rodrigueszk@users.noreply.github.com>
2022-12-02 10:35:58 -0300

    Update Exercicio03.java

--- DoWhileFor/Exercicio03.java
+++ DoWhileFor/Exercicio03.java
@@ -1,46 +1,22 @@
-import java.util.Scanner;
+package Vetores;
+
+import java.util.*;
 public class Exercicio03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner ler = new Scanner (System.in);
-		
-		int i;
-		String Mutiplode10;
-		
-		for (i=1; i<=100; i++) {
-			System.out.println(i);
-		switch(i) {
-			case 10:System.out.println(i+"Mútiplo de 10");break;
-			case 20:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 30:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 40:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 50:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 60:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 70:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 80:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 90:
-				System.out.println("Mútiplo de 10");
-				break;
-			case 100:
-				System.out.println("Mútiplo de 10");
-				break;
+			Scanner ler = new Scanner (System.in);
+			
+			int i;
+	
+			
+			for (i=1; i<=100; i++) {
+			if (i%10==0) {
+				System.out.println(i+" Mútiplo de 10!");
+			}else {
+				System.out.println(i);
 			}
-		}
+			}
 
 	}
 
